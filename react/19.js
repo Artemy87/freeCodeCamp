f9a36154af6d7a71ba85bce21a27e025f3992e94
Artemy <artemij87work@gmail.com>
2022-01-25 19:13:15 +0300

    added: react - 19

---
+++ react/19.js
@@ -0,0 +1,27 @@
+// Front end Debelopment Libraries Certification
+
+class CampSite extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div>
+        <Camper />
+      </div>
+    );
+  }
+}
+// Change code below this line
+
+const Camper = (props) => {
+  return <p>{props.name}</p>;
+};
+
+Camper.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+Camper.defaultProps = {
+  name: "CamperBot",
+};
