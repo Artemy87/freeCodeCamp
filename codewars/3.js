9b18d5cdc5c61660c7ca5e5e8bd87a163ab4b9c8
Artemy <artemij87work@gmail.com>
2022-01-29 13:33:06 +0300

    added: codewars - 3

---
+++ codewars/3.js
@@ -0,0 +1,17 @@
+const getCount = str => {
+  var vowelsCount = 0;
+  
+  let current = str.split('');
+  for (i of current) {
+    switch(i) {
+        case 'a':
+        case 'e':
+        case 'i':
+        case 'o':
+        case 'u':
+          vowelsCount++;
+          break;
+    }
+  }
+  return vowelsCount;
+}
