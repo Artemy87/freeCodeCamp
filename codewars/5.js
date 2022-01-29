34b3ab8cc669a569e89ec1a134b554a85fb4b32f
Artemy <artemij87work@gmail.com>
2022-01-30 00:10:58 +0300

    added: codewars - 6

--- codewars/5.js
+++ codewars/5.js
@@ -16,3 +16,11 @@ const rps = (p1, p2) => {
     return 'Draw!'
   }
 };
+
+// Решение с помощью обекта
+// const rps = (p1, p2) => {
+//   if (p1 === p2) return 'Draw!';
+//   return p1 === {scissors: 'paper', paper: 'rock', rock: 'scissors'}[p2]
+//     ? 'Player 2 won!'
+//     : 'Player 1 won!';
+// };
