e2f60d8715e8f1103f0236cb755d2d94eeb959f0
Artemy <artemij87work@gmail.com>
2022-02-07 21:45:41 +0300

    kata 6: Create Phone Number

--- codewars/56.js
+++ codewars/56.js
@@ -4,10 +4,4 @@ function orderFood(list) {
     !obj.hasOwnProperty(el.meal) ? obj[el.meal] = 1 : obj[el.meal] = obj[el.meal] + 1 
   })
   return obj;
-  function orderFood(list) {
-    let obj = {}
-    let arr = list.map(el => {
-      !obj.hasOwnProperty(el.meal) ? obj[el.meal] = 1 : obj[el.meal] = obj[el.meal] + 1 
-    })
-    return obj;
-  }
+};
