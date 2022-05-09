2e045b3d414ed7264ddc82fada3f0d580ddbae35
Artemy <artemij87work@gmail.com>
2022-05-09 11:10:27 +0300

    7kyu: You're a square!

--- codewars/67.js
+++ codewars/67.js
@@ -1,3 +1,7 @@
 // 7kyu: You're a square!
 
-let isSquare = n => n < 0 ? false : Number.isInteger(Math.sqrt(n))
+let isSquare = n => Number.isInteger(Math.sqrt(n))
+
+// or
+
+let isSquare2 = n => Math.sqrt(n) % 1 === 0;
